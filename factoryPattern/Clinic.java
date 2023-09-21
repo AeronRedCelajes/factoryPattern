af68cd700f830809e1cd4a4b6e4b05e907f24cd2
WindxFire <aeronredcelajes@gmail.com>
2023-09-21 20:15:07 +0800

    Added changes in the Clinic.java

--- factoryPattern/Clinic.java
+++ factoryPattern/Clinic.java
@@ -7,49 +7,58 @@ public class Clinic {
 
         Scanner input = new Scanner(System.in);
 
-        while(true){
+        while (true) {
+            try{
+                System.out.println("[1] Dog");
+                System.out.println("[2] Cat");
+                System.out.println("[3] Exit");
+                System.out.print("\nChoose your pet number: ");
+                Integer choice = input.nextInt();
 
-            System.out.println("[1] Dog");
-            System.out.println("[2] Cat");
-            System.out.println("[3] Exit");
-            System.out.print("\nChoose your pet number: ");
-            Integer choice = input.nextInt();
+                PetRecord petFile = new PetRecord();
+                Pet pet;
 
-            PetRecord petFile = new PetRecord();
-            Pet pet = null;
+                switch (choice) {
+                    case 1:
+                        pet = new Dog();
+                        petFile.setPetId("D01");
+                        petFile.setPetName("Bantay");
+                        petFile.setPet(pet);
+                        ((Dog) pet).setBreed("German Shepperd");
+                        break;
+                    case 2:
+                        pet = new Cat();
+                        petFile.setPetId("C01");
+                        petFile.setPetName("Muning");
+                        petFile.setPet(pet);
+                        ((Cat) pet).setNoOfLives(9);
+                        break;
+                    case 3:
+                        System.out.println("Exiting the program...");
+                        System.exit(0);
+                    default:
+                        System.out.println("Invalid input, please choose between 1-3.");
+                        System.out.println();
+                        continue;
+                }
 
-            switch(choice){
-                case 1: pet = new Dog();
-                    petFile.setPetId("D01");
-                    petFile.setPetName("Bantay");
-                    petFile.setPet(pet);
-                    ((Dog) pet).setBreed("German Shepperd");
-                    break;
-                case 2: pet = new Cat();
-                    petFile.setPetId("C01");
-                    petFile.setPetName("Muning");
-                    petFile.setPet(pet);
-                    ((Cat) pet).setNoOfLives(9);
-                    break;
-                case 3:
-                    System.out.println("Exiting the program...");
-                    System.exit(0);
-                default:
-                    System.out.println("Invalid input, please choose between 1-3.");
-            }
-
-            if(choice == 1 || choice == 2){
                 System.out.println("Pet id is " + petFile.getPetId());
                 System.out.println("Pet name is " + petFile.getPetName());
                 System.out.println("Pet kind: " + petFile.getPet().getClass().getSimpleName());
 
-                if(choice == 1)
+                if (choice == 1)
                     System.out.println("Pet breed: " + ((Dog) pet).getBreed());
                 else
                     System.out.println("Pet no. of lives: " + ((Cat) pet).getNoOfLives());
 
-                System.out.println("Communication sound: "+ petFile.getPet().makeSound());
+                System.out.println("Communication sound: " + petFile.getPet().makeSound());
                 System.out.println("Play mode: " + petFile.getPet().play());
+                System.out.println();
+
+            }catch(Exception e) {
+                System.out.println("Error, invalid input!");
+                System.out.println();
+                input.nextLine();
             }
         }
     }
